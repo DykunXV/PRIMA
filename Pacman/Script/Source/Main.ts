c74f97691828aa286b0393efa010f415dd7b1a27
Dennis Fischer <dykunxv@gmail.com>
2022-03-24 12:07:17 +0100

    changed graph

--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -3,10 +3,16 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let pacman: ƒ.Node;
+  let speed: ƒ.Vector3 = new ƒ.Vector3(1/60, 1/60, 0)
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    let graph: ƒ.Node = viewport.getBranch();
+    pacman = graph.getChildrenByName("Pacman")[0];
+    console.log(pacman);
+    
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -14,6 +20,7 @@ namespace Script {
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    pacman.mtxLocal.translate(speed);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
