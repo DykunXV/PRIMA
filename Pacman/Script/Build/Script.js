d62bef6b48cc36d1695375231c0fd6e364ceb57b
Dennis Fischer <dykunxv@gmail.com>
2022-04-05 10:16:53 +0200

    copied jirkas pacman as the code is much simpler

--- Pacman/Script/Build/Script.js
+++ Pacman/Script/Build/Script.js
@@ -39,141 +39,68 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
-    ƒ.Debug.info('Main Program Template running!');
+    ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let pacman;
     let grid;
-    let nextRow;
-    let nextRowTile;
-    let speed = new ƒ.Vector3(0, 0, 0);
-    let pacmanCurrentX = 1;
-    let pacmanCurrentY = 1;
-    let WalkingDirections;
-    (function (WalkingDirections) {
-        WalkingDirections["None"] = "NONE";
-        WalkingDirections["Up"] = "UP";
-        WalkingDirections["Down"] = "DOWN";
-        WalkingDirections["Left"] = "LEFT";
-        WalkingDirections["Right"] = "RIGHT";
-    })(WalkingDirections || (WalkingDirections = {}));
-    let currentWalkingDirection = 'NONE';
-    let newWalkingDirection = 'NONE';
-    document.addEventListener('interactiveViewportStarted', start);
+    let direction = ƒ.Vector2.ZERO();
+    let speed = 0.05;
+    let waka;
+    document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
+        console.log(viewport.camera);
+        viewport.camera.mtxPivot.translateZ(10);
+        viewport.camera.mtxPivot.rotateY(180);
+        viewport.camera.mtxPivot.translateX(-2);
+        viewport.camera.mtxPivot.translateY(2);
         let graph = viewport.getBranch();
-        pacman = graph.getChildrenByName('Pacman')[0];
+        pacman = graph.getChildrenByName("Pacman")[0];
+        grid = graph.getChildrenByName("Grid")[0];
+        console.log(pacman);
+        ƒ.AudioManager.default.listenTo(graph);
+        waka = graph.getChildrenByName("Sound")[0].getComponents(ƒ.ComponentAudio)[1];
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
-    function isNextTileWall(walkingDirection) {
-        let graph = viewport.getBranch();
-        grid = graph.getChildrenByName('Grid')[0];
-        switch (walkingDirection) {
-            case 'LEFT':
-                nextRow = grid.getChildren()[pacmanCurrentY];
-                nextRowTile = nextRow.getChildren()[pacmanCurrentX - 1];
-                if (nextRowTile.name == 'Wall') {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            case 'RIGHT':
-                nextRow = grid.getChildren()[pacmanCurrentY];
-                nextRowTile = nextRow.getChildren()[pacmanCurrentX + 1];
-                if (nextRowTile.name == 'Wall') {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            case 'UP':
-                nextRow = grid.getChildren()[pacmanCurrentY + 1];
-                nextRowTile = nextRow.getChildren()[pacmanCurrentX];
-                if (nextRowTile.name == 'Wall') {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            case 'DOWN':
-                nextRow = grid.getChildren()[pacmanCurrentY - 1];
-                nextRowTile = nextRow.getChildren()[pacmanCurrentX];
-                if (nextRowTile.name == 'Wall') {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            default:
-                return false;
-        }
-    }
-    function setNewWalkingDirection() {
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])) {
-            newWalkingDirection = WalkingDirections.Left;
-        }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D])) {
-            newWalkingDirection = WalkingDirections.Right;
-        }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])) {
-            newWalkingDirection = WalkingDirections.Up;
-        }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])) {
-            newWalkingDirection = WalkingDirections.Down;
-        }
-    }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
-        setNewWalkingDirection();
-        console.log(pacman.mtxLocal.translation.y);
-        if (newWalkingDirection == 'RIGHT' &&
-            pacman.mtxLocal.translation.y % 1 < 0.05 &&
-            isNextTileWall(newWalkingDirection) == false) {
-            currentWalkingDirection = WalkingDirections.Right;
-            speed = new ƒ.Vector3(1 / 60, 0, 0);
+        let posPacman = pacman.mtxLocal.translation;
+        let nearestGridPoint = new ƒ.Vector2(Math.round(posPacman.x), Math.round(posPacman.y));
+        let nearGridPoint = posPacman.toVector2().equals(nearestGridPoint, 2 * speed);
+        if (nearGridPoint) {
+            let directionOld = direction.clone;
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+                direction.set(1, 0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
+                direction.set(-1, 0);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]))
+                direction.set(0, 1);
+            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]))
+                direction.set(0, -1);
+            if (blocked(ƒ.Vector2.SUM(nearestGridPoint, direction)))
+                if (direction.equals(directionOld)) // did not turn
+                    direction.set(0, 0); // full stop
+                else {
+                    if (blocked(ƒ.Vector2.SUM(nearestGridPoint, directionOld))) // wrong turn and dead end
+                        direction.set(0, 0); // full stop
+                    else
+                        direction = directionOld; // don't turn but continue ahead
+                }
+            if (!direction.equals(directionOld) || direction.equals(ƒ.Vector2.ZERO()))
+                pacman.mtxLocal.translation = nearestGridPoint.toVector3();
+            if (direction.equals(ƒ.Vector2.ZERO()))
+                waka.play(false);
+            else if (!waka.isPlaying)
+                waka.play(true);
         }
-        if (newWalkingDirection == 'LEFT' &&
-            pacman.mtxLocal.translation.y % 1 < 0.05 &&
-            isNextTileWall(newWalkingDirection) == false) {
-            currentWalkingDirection = WalkingDirections.Left;
-            speed = new ƒ.Vector3(-1 / 60, 0, 0);
-        }
-        if (newWalkingDirection == 'UP' &&
-            pacman.mtxLocal.translation.x % 1 < 0.05 &&
-            isNextTileWall(newWalkingDirection) == false) {
-            currentWalkingDirection = WalkingDirections.Up;
-            speed = new ƒ.Vector3(0, 1 / 60, 0);
-        }
-        if (newWalkingDirection == 'DOWN' &&
-            pacman.mtxLocal.translation.x % 1 < 0.05 &&
-            isNextTileWall(newWalkingDirection) == false) {
-            currentWalkingDirection = WalkingDirections.Down;
-            speed = new ƒ.Vector3(0, -1 / 60, 0);
-        }
-        if (pacman.mtxLocal.translation.y >= pacmanCurrentY + 1) {
-            pacmanCurrentY += 1;
-            console.log('Y: ' + pacmanCurrentY);
-        }
-        if (pacman.mtxLocal.translation.y <= pacmanCurrentY - 1) {
-            pacmanCurrentY -= 1;
-            console.log('Y: ' + pacmanCurrentY);
-        }
-        if (pacman.mtxLocal.translation.x >= pacmanCurrentX + 1) {
-            pacmanCurrentX += 1;
-            console.log('X: ' + pacmanCurrentX);
-        }
-        if (pacman.mtxLocal.translation.x <= pacmanCurrentX - 1) {
-            pacmanCurrentX -= 1;
-            console.log('X: ' + pacmanCurrentX);
-        }
-        if (isNextTileWall(currentWalkingDirection)) {
-            speed = new ƒ.Vector3(0, 0, 0);
-        }
-        pacman.mtxLocal.translate(speed);
+        pacman.mtxLocal.translate(ƒ.Vector2.SCALE(direction, speed).toVector3());
         viewport.draw();
-        ƒ.AudioManager.default.update();
+        // ƒ.AudioManager.default.update();
+    }
+    function blocked(_posCheck) {
+        let check = grid.getChild(_posCheck.y)?.getChild(_posCheck.x)?.getChild(0);
+        return (!check || check.name == "Wall");
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -1,171 +1,84 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info('Main Program Template running!');
+  ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
   let grid: ƒ.Node;
-  let nextRow: ƒ.Node;
-  let nextRowTile: ƒ.Node;
-  let speed: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
-  let pacmanCurrentX: number = 1;
-  let pacmanCurrentY: number = 1;
-  enum WalkingDirections {
-    None = 'NONE',
-    Up = 'UP',
-    Down = 'DOWN',
-    Left = 'LEFT',
-    Right = 'RIGHT',
-  }
+  let direction: ƒ.Vector2 = ƒ.Vector2.ZERO();
+  let speed: number = 0.05;
+  let waka: ƒ.ComponentAudio;
 
-  let currentWalkingDirection = 'NONE';
-  let newWalkingDirection = 'NONE';
-
-  document.addEventListener('interactiveViewportStarted', <EventListener>start);
+  document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+
+
+    console.log(viewport.camera);
+    viewport.camera.mtxPivot.translateZ(10);
+    viewport.camera.mtxPivot.rotateY(180);
+    viewport.camera.mtxPivot.translateX(-2);
+    viewport.camera.mtxPivot.translateY(2);
+
+
     let graph: ƒ.Node = viewport.getBranch();
-    pacman = graph.getChildrenByName('Pacman')[0];
+    pacman = graph.getChildrenByName("Pacman")[0];
+    grid = graph.getChildrenByName("Grid")[0];
+    console.log(pacman);
+
+    ƒ.AudioManager.default.listenTo(graph);
+    waka = graph.getChildrenByName("Sound")[0].getComponents(ƒ.ComponentAudio)[1];
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-  }
-
-  function isNextTileWall(walkingDirection: string): boolean {
-    let graph: ƒ.Node = viewport.getBranch();
-    grid = graph.getChildrenByName('Grid')[0];
-
-    switch (walkingDirection) {
-      case 'LEFT':
-        nextRow = grid.getChildren()[pacmanCurrentY];
-        nextRowTile = nextRow.getChildren()[pacmanCurrentX - 1];
-        if (nextRowTile.name == 'Wall') {
-          return true;
-        } else {
-          return false;
-        }
-      case 'RIGHT':
-        nextRow = grid.getChildren()[pacmanCurrentY];
-        nextRowTile = nextRow.getChildren()[pacmanCurrentX + 1];
-        if (nextRowTile.name == 'Wall') {
-          return true;
-        } else {
-          return false;
-        }
-      case 'UP':
-        nextRow = grid.getChildren()[pacmanCurrentY + 1];
-        nextRowTile = nextRow.getChildren()[pacmanCurrentX];
-        if (nextRowTile.name == 'Wall') {
-          return true;
-        } else {
-          return false;
-        }
-      case 'DOWN':
-        nextRow = grid.getChildren()[pacmanCurrentY - 1];
-        nextRowTile = nextRow.getChildren()[pacmanCurrentX];
-        if (nextRowTile.name == 'Wall') {
-          return true;
-        } else {
-          return false;
-        }
-      default:
-        return false;
-    }
-  }
-
-  function setNewWalkingDirection(): void {
-    if (
-      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])
-    ) {
-      newWalkingDirection = WalkingDirections.Left;
-    }
-    if (
-      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D])
-    ) {
-      newWalkingDirection = WalkingDirections.Right;
-    }
-    if (
-      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])
-    ) {
-      newWalkingDirection = WalkingDirections.Up;
-    }
-    if (
-      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])
-    ) {
-      newWalkingDirection = WalkingDirections.Down;
-    }
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    let posPacman: ƒ.Vector3 = pacman.mtxLocal.translation;
+    let nearestGridPoint: ƒ.Vector2 = new ƒ.Vector2(Math.round(posPacman.x), Math.round(posPacman.y));
+    let nearGridPoint: boolean = posPacman.toVector2().equals(nearestGridPoint, 2 * speed);
 
-    setNewWalkingDirection();
+    if (nearGridPoint) {
+      let directionOld: ƒ.Vector2 = direction.clone;
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+        direction.set(1, 0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
+        direction.set(-1, 0);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]))
+        direction.set(0, 1);
+      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]))
+        direction.set(0, -1);
 
-    console.log(pacman.mtxLocal.translation.y)
 
-    if (
-      newWalkingDirection == 'RIGHT' &&
-      pacman.mtxLocal.translation.y % 1 < 0.05 &&
-      isNextTileWall(newWalkingDirection) == false
-    ) {
-      currentWalkingDirection = WalkingDirections.Right;
-      speed = new ƒ.Vector3(1 / 60, 0, 0);
-    }
-    if (
-      newWalkingDirection == 'LEFT' &&
-      pacman.mtxLocal.translation.y % 1 < 0.05 &&
-      isNextTileWall(newWalkingDirection) == false
-    ) {
-      currentWalkingDirection = WalkingDirections.Left;
-      
-      speed = new ƒ.Vector3(-1 / 60, 0, 0);
-    }
-    if (
-      newWalkingDirection == 'UP' &&
-      pacman.mtxLocal.translation.x % 1 < 0.05 &&
-      isNextTileWall(newWalkingDirection) == false
-    ) {
-      currentWalkingDirection = WalkingDirections.Up;
-      speed = new ƒ.Vector3(0, 1 / 60, 0);
-    }
-    if (
-      newWalkingDirection == 'DOWN' &&
-      pacman.mtxLocal.translation.x % 1 < 0.05 &&
-      isNextTileWall(newWalkingDirection) == false
-    ) {
-      currentWalkingDirection = WalkingDirections.Down;
-      speed = new ƒ.Vector3(0, -1 / 60, 0);
+      if (blocked(ƒ.Vector2.SUM(nearestGridPoint, direction)))
+        if (direction.equals(directionOld)) // did not turn
+          direction.set(0, 0); // full stop
+        else {
+          if (blocked(ƒ.Vector2.SUM(nearestGridPoint, directionOld))) // wrong turn and dead end
+            direction.set(0, 0); // full stop
+          else
+            direction = directionOld; // don't turn but continue ahead
+        }
+
+      if (!direction.equals(directionOld) || direction.equals(ƒ.Vector2.ZERO()))
+        pacman.mtxLocal.translation = nearestGridPoint.toVector3();
+
+      if (direction.equals(ƒ.Vector2.ZERO()))
+        waka.play(false);
+      else if (!waka.isPlaying)
+        waka.play(true);
+
     }
 
-    if (pacman.mtxLocal.translation.y >= pacmanCurrentY + 1) {
-      pacmanCurrentY += 1;
-      console.log('Y: ' + pacmanCurrentY);
-    }
-
-    if (pacman.mtxLocal.translation.y <= pacmanCurrentY - 1) {
-      pacmanCurrentY -= 1;
-      console.log('Y: ' + pacmanCurrentY);
-    }
-
-    if (pacman.mtxLocal.translation.x >= pacmanCurrentX + 1) {
-      pacmanCurrentX += 1;
-      
-      console.log('X: ' + pacmanCurrentX);
-    }
-
-    if (pacman.mtxLocal.translation.x <= pacmanCurrentX - 1) {
-      pacmanCurrentX -= 1;
-      
-      console.log('X: ' + pacmanCurrentX);
-    }
-    
-    if (isNextTileWall(currentWalkingDirection)) {
-      speed = new ƒ.Vector3(0, 0, 0);
-    }
-
-    pacman.mtxLocal.translate(speed);
+    pacman.mtxLocal.translate(ƒ.Vector2.SCALE(direction, speed).toVector3());
     viewport.draw();
-    ƒ.AudioManager.default.update();
+    // ƒ.AudioManager.default.update();
+  }
+
+  function blocked(_posCheck: ƒ.Vector2): boolean {
+    let check: ƒ.Node = grid.getChild(_posCheck.y)?.getChild(_posCheck.x)?.getChild(0);
+    return (!check || check.name == "Wall");
   }
 }
